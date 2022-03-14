7c9cc970b77f8b50cdc3d47eb98dc35b57119efe
Jitendra Pandey <jitendrap@chetu.com>
2022-03-15 00:34:46 +0530

    Update install_composer_dependencies.sh

--- scripts/install_composer_dependencies.sh
+++ scripts/install_composer_dependencies.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 cd /var/www/html
 yum install wget -y
+sudo yum install php-cli php-xml php-json php-mbstring php-process php-common php-fpm php-zip php-mysqlnd git -y
+curl -sS https://getcomposer.org/installer | php
+sudo mv composer.phar /usr/bin/composer
+sudo chmod +x /usr/bin/composer
+#
 wget https://getcomposer.org/composer.phar
 php composer.phar install
+
+curl -sS https://getcomposer.org/installer | php
+sudo mv composer.phar /usr/bin/composer
+sudo chmod +x /usr/bin/composer
